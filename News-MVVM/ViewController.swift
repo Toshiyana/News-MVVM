74f9d8e367e0ca89059e5e8fa2c421b4c4205892
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-12-11 02:04:16 +0900

    Initial Commit

---
+++ News-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News-MVVM
+//
+//  Created by Toshiyana on 2021/12/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
